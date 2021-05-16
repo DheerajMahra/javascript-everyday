5a52e6fdcb05698ba5a68e09f5efecf5c4b80699
Dheeraj Mahra <dheerajmahra873@gmail.com>
2021-05-16 21:20:49 +0530

    [closure] Only values used by the inner fn will be in the closure

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ let incrementCount = counter();
 incrementCount(); //count updated to 1 and remains in the memory
 incrementCount(); //now, count = 1 will be used here.
 incrementCount(); // count = 3
+
+//#2 Only values used by the inner function will be present in the closure
+function helloClosure() {
+  let greet = "Hello World!";
+  let enjoy = "Wohooo!";
+  let goodbye = "Bye!";
+
+  return function logger() {
+    console.log(greet);
+    console.log(enjoy);
+  };
+}
+
+let log = helloClosure();
+log(); //goodbye will not be in the closure
