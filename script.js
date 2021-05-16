8742e17b8a07469904271ae034a706b638dbb2ba
Dheeraj Mahra <dheerajmahra873@gmail.com>
2021-05-16 21:49:26 +0530

    simplest example of closure in js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-//#1 Closures values are retained and updated values are used everytime.
+//#1 Simplest example of a closure in js
+function liveADay() {
+  let food = "cheese";
+  function eat() {
+    console.log(food + " is good");
+  }
+  eat(); //See, a closure! Eat closes over food
+}
+liveADay();
+
+//#2 Closures values are retained and updated values are used everytime.
 function counter() {
   let count = 0;
 
@@ -13,7 +23,7 @@ incrementCount(); //count updated to 1 and remains in the memory
 incrementCount(); //now, count = 1 will be used here.
 incrementCount(); // count = 3
 
-//#2 Only values used by the inner function will be present in the closure
+//#3 Only values used by the inner function will be present in the closure
 function helloClosure() {
   let greet = "Hello World!";
   let enjoy = "Wohooo!";
