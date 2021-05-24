9405411be7cf596e6e853ce386685f0a0b0579dd
Dheeraj Mahra <dheerajmahra873@gmail.com>
2021-05-25 01:06:22 +0530

    closure loop with setTimeout

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //#1 Simplest example of a closure in js
-function liveADay() {
+/*function liveADay() {
   let food = "cheese";
   function eat() {
     console.log(food + " is good");
   }
   eat(); //See, a closure! Eat closes over food
 }
-liveADay();
+liveADay();*/
 
 //#2 Closures values are retained and updated values are used everytime.
-function counter() {
+/*function counter() {
   let count = 0;
 
   return function increment() {
@@ -21,10 +21,10 @@ function counter() {
 let incrementCount = counter();
 incrementCount(); //count updated to 1 and remains in the memory
 incrementCount(); //now, count = 1 will be used here.
-incrementCount(); // count = 3
+incrementCount(); // count = 3*/
 
 //#3 Only values used by the inner function will be present in the closure
-function helloClosure() {
+/*function helloClosure() {
   let greet = "Hello World!";
   let enjoy = "Wohooo!";
   let goodbye = "Bye!";
@@ -36,4 +36,37 @@ function helloClosure() {
 }
 
 let log = helloClosure();
-log(); //goodbye will not be in the closure
+log(); //goodbye will not be in the closure*/
+
+//#4 This will print 6 6 6 6. Closure!
+/*function printNum1() {
+  for (var i = 0; i < 6; i++) {
+    setTimeout(function () {
+      console.log(i); //each fn maintains a reference of i, i becomes 6 after loop
+    }, i * 1000);
+  }
+}
+printNum1();*/
+
+//#5 Print number after interval of 1s
+/*function printNum2() {
+  for (let i = 0; i < 6; i++) {
+    setTimeout(function () {
+      console.log(i); //let will create a new i for each iteration
+    }, i * 1000);
+  }
+}
+printNum2();*/
+
+//#6 Print number after interval of 1s without using let
+/*function printNum3() {
+  for (var i = 0; i < 6; i++) {
+    function close(j) {
+      setTimeout(function () {
+        console.log(j); //we just need to create a separate variable for every iteration
+      }, j * 1000);
+    }
+    close(i); //Fn will help us to do that
+  }
+}
+printNum3();*/
